9ed9324dde380f7fd16a92c97f8b04790af9462b
alexisabels <isabelsanchezalex@gmail.com>
2024-06-14 18:15:35 +0200

    cosillas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			backgroundImage: {
-				'light-gradient': "radial-gradient(ellipse 80% 80% at 50% -20%, rgba(217,216,255,0.9), rgba(255,255,255,0.9)",
-				'dark-gradient': "radial-gradient(ellipse 80% 80% at 50% -20%, rgba(120,119,198,0.9), rgba(255,255,255,0))"
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundImage: {
+        "light-gradient": "#ffff",
+        "dark-gradient": "#161616)",
+      },
+    },
+  },
+  plugins: [],
+};
