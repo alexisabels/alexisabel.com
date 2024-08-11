2eb8e11cea9be4bbd0546473115171264d9a175c
alexisabels <isabelsanchezalex@gmail.com>
2024-08-12 01:25:55 +0200

    Output hybrid fix test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
