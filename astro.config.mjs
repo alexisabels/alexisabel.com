caeea5c136106158e0a2bc59dea2e3ab46f099dc
alexisabels <isabelsanchezalex@gmail.com>
2024-11-19 20:13:28 +0100

    new cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,14 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://www.alexisabel.com",
   output: "hybrid",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+  i18n: {
+    defaultLocale: "es", 
+    locales: ["es", "en"],
+
+  },
 });
