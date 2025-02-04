9a3b1058da6de4b1ffabe8db0edf15debcafeb9a
alexisabels <isabelsanchezalex@gmail.com>
2025-02-04 17:00:29 +0100

    tooltip added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+
   darkMode: "class",
   theme: {
     extend: {
@@ -10,5 +14,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
